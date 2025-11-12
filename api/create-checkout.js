d65b16c6dfe9792b9bdb8657e42e6ac2c07cf063
internetamy <internetamy@gmail.com>
2025-11-11 16:00:54 -0800

    UPdate Name to price

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -2,7 +2,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const NAME_TO_PRICEID = {
   "GLOW": "price_1SrjPqCtq6MyYdzb3guUnv",
-  "R3 - 10":"price_1SSRHXCNw9KMO1UhsdNBut2K",
+  "AXQRN":"price_1SSRHXCNw9KMO1UhsdNBut2K",
   "BPC-157 10mg": "price_1Srj91Ctq6MyYdzbU92PDc"
 };
 
